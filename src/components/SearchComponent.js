2a401ca9afc41a55e0787d094ef1e30205a2b663
Gonzalo Amadio <gonzaloamadio@gmail.com>
2019-07-17 17:04:04 +1000

    Fix "no results" message on initial render

--- src/components/SearchComponent.js
+++ src/components/SearchComponent.js
@@ -4,10 +4,16 @@ import SearchResults from './SearchResults'
 
 export default class SearchComponent extends Component {
     
-    state = {results: []}
+    state = {results: [], usedSearch: false}
 
     _handleResults = (results) => {
-      this.setState({results})
+      this.setState({results, usedSearch:true})
+    }
+
+    _renderResults(){
+        return this.state.results.length === 0
+            ? <p>- No Results -</p>
+            : <SearchResults results={this.state.results}/>
     }
 
     render() {
@@ -16,10 +22,10 @@ export default class SearchComponent extends Component {
             <div className='SearchForm-wrapper'>
             <SearchFormController onResults={this._handleResults}/>
             </div>
-            {this.state.results.length === 0
-            ? <p>Sin Resultados</p>
-            : <SearchResults results={this.state.results}/>
-            } 
+            {this.state.usedSearch === false
+            ? <small>Use the search bar to sear for movie by title</small>
+            : this._renderResults()
+            }
         </div>
         )
     }
